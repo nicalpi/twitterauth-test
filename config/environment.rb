e5217424a80552426961aa09371ad5b8db2e1615
alpin <alpin@alpin-desktop.(none)>
2009-05-08 09:05:21 +0100

    Install twitterauth

--- config/environment.rb
+++ config/environment.rb
@@ -19,9 +19,10 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-    config.gem "thoughtbot-shoulda", :lib => "shoulda", :source => "http://gems.github.com"
-    config.gem "thoughtbot-factory_girl",:lib    => "factory_girl",:source => "http://gems.github.com"
-    config.gem "haml"
+  config.gem "thoughtbot-shoulda", :lib => "shoulda", :source => "http://gems.github.com"
+  config.gem "thoughtbot-factory_girl",:lib    => "factory_girl",:source => "http://gems.github.com"
+  config.gem "haml"
+  config.gem 'twitter-auth', :lib => 'twitter_auth'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
