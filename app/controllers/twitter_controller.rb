e637c85eeb4f985650a943ff4057bc756b4e8bca
alpin <alpin@alpin-desktop.(none)>
2009-05-11 09:04:36 +0100

    Add comment

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,10 +1,10 @@
 class TwitterController < ApplicationController
     before_filter :login_required
-
+    #Retreive the friend timeline
     def index
       @tweets = current_user.twitter.get('/statuses/friends_timeline')
       logger.info @tweets
-
     end
+
 end
 
